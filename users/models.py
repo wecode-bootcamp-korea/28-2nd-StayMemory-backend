ae04446af485daa262ee5876209ee55c028c84bb
이찬영 <chan@ichan-yeongs-MacBook-Pro.local>
2022-01-11 17:28:11 +0900

    Modify : user models.py

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from utils.time_stamp_model import TimeStampModel
 class User(TimeStampModel):
     kakao_id = models.CharField(max_length=25, unique=True)
     nickname = models.CharField(max_length=50)
-    email    = models.CharField(max_length=30)
-    gender   = models.IntegerField()
+    email    = models.CharField(max_length=30, null=True)
+    gender   = models.IntegerField(null=True)
 
     class Meta:
         db_table = "users"
